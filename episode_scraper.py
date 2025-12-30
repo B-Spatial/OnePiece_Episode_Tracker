6d827fc5f02b00f76c0b2280c1452ba36ea0e93d
B-Spatial <larsenbrandont@gmail.com>
2025-12-29 17:13:04 -0800

    added function to episode_scraper.py file

--- episode_scraper.py
+++ episode_scraper.py
@@ -1,8 +1,22 @@
 import pandas as pd
+'''
 url = "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_1%E2%80%938)#Series_overview"
 all_tables = pd.read_html(url, storage_options={
         "User-Agent": "Chrome/120.0.0.0"})
 
 #print(all_tables[1])
-df = all_tables[2]
-df.to_csv("one_piece_season_1.csv", index=False)
+df = all_tables[7]
+df.to_csv("one_piece_season_6.csv", index=False)
+'''
+
+def episode_scrape(url_links):
+    for url in url_links:
+        table = pd.read_html(url, flavor="lxml", storage_options={"User-Agent": "Chrome/120.0.0.0"})
+
+        df = pd.concat(table, ignore_index=True)
+
+        name = url.split("/")[-1]
+        df.to_csv(f"{name}.csv", index=False, encoding="utf-8-sig")
+
+        print(f"Saved {name}.csv")
+    
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import pandas as pd
+from episode_scraper import episode_scrape
+
 def main():
-    print("Hello from onepiece-episode-tracker!")
+   url_list = ["https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_1%E2%80%938)#Series_overview", 
+               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_9%E2%80%9314)",
+               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_15%E2%80%9319)#Season_18:_Zou_(2016%E2%80%9317)",
+               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_20%E2%80%93present)"
+               ]
+   episode_scrape(url_list)
 
 
 if __name__ == "__main__":
