707267a84a34a8b2135e071880211779f7742129
B-Spatial <larsenbrandont@gmail.com>
2026-01-05 14:00:53 -0800

    Updated episode_scrape function. Now accounts for only episode tables by using a dictionary method.

--- episode_scraper.py
+++ episode_scraper.py
@@ -1,33 +1,22 @@
 import pandas as pd
-'''
-url = "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_1%E2%80%938)#Series_overview"
-all_tables = pd.read_html(url, storage_options={
-        "User-Agent": "Chrome/120.0.0.0"})
 
-#print(all_tables[1])
-df = all_tables[7]
-df.to_csv("one_piece_season_6.csv", index=False)
-'''
 
-def episode_scrape(url_links):
-    count = 0
+def episode_scrape(url_dict):
+    for url, table_slice in url_dict.items():
+        tables = pd.read_html(
+            url,
+            flavor="lxml",
+            storage_options={"User-Agent": "Chrome/120.0.0.0"}
+        )
 
-    for url in url_links:
-        table = pd.read_html(url, flavor="lxml", storage_options={"User-Agent": "Chrome/120.0.0.0"})
-
-        if count == 0:
-            table = table[:1]
-        else:
-            table = table[2:]
-
-        df = pd.concat(table, ignore_index=True)
+        # Apply the slice for this URL
+        tables = tables[table_slice]
 
         name = url.split("/")[-1]
 
-        for i, df in enumerate(table, start=1):
+        for i, df in enumerate(tables, start=1):
             filename = f"{name}_table_{i}.csv"
             df.to_csv(filename, index=False, encoding="utf-8-sig")
             print(f"Saved {filename}")
 
-        count += 1
     
--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import pandas as pd
 from episode_scraper import episode_scrape
 
 def main():
+   '''
    url_list = ["https://en.wikipedia.org/wiki/Lists_of_One_Piece_episodes",
                "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_1%E2%80%938)#Series_overview", 
                "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_9%E2%80%9314)",
                "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_15%E2%80%9319)#Season_18:_Zou_(2016%E2%80%9317)",
                "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_20%E2%80%93present)"
                ]
-   episode_scrape(url_list)
+    '''
+   
+   url_dict = {"https://en.wikipedia.org/wiki/Lists_of_One_Piece_episodes": slice(0, 1),
+               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_1%E2%80%938)#Season_1:_East_Blue_(1999%E2%80%932001)": slice(2, 10),
+               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_9%E2%80%9314)": slice(1, 7),
+               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_15%E2%80%9319)#Season_18:_Zou_(2016%E2%80%9317)": slice(1, 6),
+               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_20%E2%80%93present)": slice(1, 3)
+               }
+
+   episode_scrape(url_dict)
 
 
 if __name__ == "__main__":
