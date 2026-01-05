fd10fd98782e5a59fe407c5da782000beb6a5822
B-Spatial <larsenbrandont@gmail.com>
2026-01-05 14:01:52 -0800

    Cleaned up main file.

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ import pandas as pd
 from episode_scraper import episode_scrape
 
 def main():
-   '''
-   url_list = ["https://en.wikipedia.org/wiki/Lists_of_One_Piece_episodes",
-               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_1%E2%80%938)#Series_overview", 
-               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_9%E2%80%9314)",
-               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_15%E2%80%9319)#Season_18:_Zou_(2016%E2%80%9317)",
-               "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_20%E2%80%93present)"
-               ]
-    '''
    
    url_dict = {"https://en.wikipedia.org/wiki/Lists_of_One_Piece_episodes": slice(0, 1),
                "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_(seasons_1%E2%80%938)#Season_1:_East_Blue_(1999%E2%80%932001)": slice(2, 10),
